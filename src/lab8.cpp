883c42c000ce49c718269eddd4c355a15ddc65ed
jeanwang325 <jeanwang325@gmail.com>
2016-05-18 02:18:50 -0500

    greater_val and pointer_fun

--- src/lab8.cpp
+++ src/lab8.cpp
@@ -7,15 +7,25 @@ short greater_val(shared_ptr<short>, shared_ptr<short>);
 int main()
 {
     cout << "main()" << endl;
+    pointer_fun(3,6);
+
+    std::shared_ptr<short> pa(new short(3));
+    std::shared_ptr<short> pb(new short(9));
+    short val = greater_val(pa, pb);
+    std::cout << "Greater_val test: " << val  << endl;
+
 
 }
 
 void pointer_fun(int a, int b)
 {
-
+    std::shared_ptr<int> pa = std::make_shared<int>(a);
+    std::shared_ptr<int> pb = std::make_shared<int>(b);
+    std::cout << *pa << " " << *pb << endl;
 }
 
 short greater_val(shared_ptr<short> a, shared_ptr<short> b)
 {
-    return 0; //fix this
+    return *a > *b? *a : *b;
+
 }
