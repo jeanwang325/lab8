c6e495a09f8b322c8cd8c8afdb1a524cd90240ca
jeanwang325 <jeanwang325@gmail.com>
2016-05-18 02:37:36 -0500

    added set

--- src/lab8.cpp
+++ src/lab8.cpp
@@ -1,5 +1,5 @@
 #include "eecs230.h"
-
+#include <set>
 //function declarations
 void pointer_fun(int, int);
 short greater_val(shared_ptr<short>, shared_ptr<short>);
@@ -15,6 +15,25 @@ int main()
     std::cout << "Greater_val test: " << val  << endl;
 
 
+//    set<string> people;
+//    people.insert("Tommy");
+//    std::string myname = "Jill";
+//    people.insert(myname);
+//    people.insert("Johnny");
+//    for(set<string>::iterator it=people.begin();
+//        it != people.end();
+//        ++it){
+//        cout << "Person: " << *it << endl;
+//    }
+//    myname = "Tommy";
+//    if(people.find(myname) != people.end()){
+//        cout<< "Tommy is a CS/CECS major!" << endl;
+//    }
+//    else {
+//        cout<< "Tommy is the wrong major!" << endl;
+//    }
+//    myname = "Johnny";
+//    people.erase("Johnny");
 }
 
 void pointer_fun(int a, int b)
